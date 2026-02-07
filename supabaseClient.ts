552790a0a825234a640eae1ddd6f42ed9725b5fa
E Soft <esfotofficialbd@gmail.com>
2026-02-07 15:52:28 +0600

    Update supabaseClient.ts

--- supabaseClient.ts
+++ supabaseClient.ts
@@ -2,6 +2,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 export const supabaseUrl = 'https://vzriehbewfrsnoxezatp.supabase.co';
-export const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ6cmllaGJld2Zyc25veGV6YXRwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk5MjA3MzYsImV4cCI6MjA4NTQ5NjczNn0.khO5SGpPzpwZrdkUHlqv14RTKRSu6ujq8p_SStmrMGI';
+export const supabaseAnonKey = 'sb_publishable_5reruEzouT75dWG1yj9zOA_9byfcdSs';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
